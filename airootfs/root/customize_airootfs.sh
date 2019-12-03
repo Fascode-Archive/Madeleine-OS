6fa8f1b7c21b9c2e8b2d2409895d1af735cce0c6
hayao <shun819.mail@gmail.com>
2019-12-03 19:49:15 +0000

    起動できないバグを修正

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -25,7 +25,7 @@ rm /usr/share/backgrounds/xfce/xfce-stripes.png
 ln -s /usr/share/backgrounds/cirno.jpg /usr/share/backgrounds/xfce/xfce-stripes.png
 chmod 644 /usr/share/backgrounds/cirno.jpg
 if [[ -d /usr/share/calamares/branding/manjaro ]]; then
-    rm -rf /usr/share/calamares/branding/
+    rm -rf /usr/share/calamares/branding/manjaro
 fi
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
